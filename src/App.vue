e864760355f6c63bdbc822d4ae8738f9d6f61104
Fares Gdoura <gdourafares@gmail.com>
2024-06-04 16:19:01 +0400

    create-movie-rating-app: add starting boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,46 @@
 <script setup>
 import { items } from "./movies.json";
-/*
- This is an Icon that you can use to represent the stars if you like
- otherwise you could just use a simple ⭐️ emoji, or * character.
-*/
-// import { StarIcon } from "@heroicons/vue/24/solid";
+import {ref} from "vue";
+import { StarIcon } from "@heroicons/vue/24/solid";
+
+const movies = ref(items)
 </script>
 
 <template>
-  <div></div>
+  <div class="app">
+    <div class="movie-list">
+      <div class="movie-item" v-for="movie in movies" :key="movie.id">
+        <div class="movie-item-image-wrapper">
+          <img :src="movie.image" class="movie-item-image" alt="" />
+        </div>
+
+        <div class="movie-item-content-wrapper">
+          <div class="movie-item-title-wrapper">
+            <h3 class="movie-item-title">{{ movie.name }}</h3>
+            <div class="movie-item-genres-wrapper">
+              <span
+                  v-for="genre in movie.genres"
+                  :key="`${movie.id}-${genre}`"
+                  class="movie-item-genre-tag"
+              >{{ genre }}</span
+              >
+            </div>
+          </div>
+          <div class="movie-item-description-wrapper">
+            <p class="movie-item-description">{{ movie.description }}</p>
+          </div>
+          <div class="movie-item-rating-wrapper">
+            <span class="movie-item-rating-text">
+              Rating: ({{ movie.rating }}/5)
+            </span>
+            <StarIcon
+                v-for="star in movie.rating"
+                :key="`star-${star}`"
+                class="movie-item-star-icon"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
